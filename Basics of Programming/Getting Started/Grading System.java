1644d9cab0fa477b048cbdc3f5101341e5ec4985
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-02-25 16:55:33 +0530

    Create Grading System.java

---
+++ Basics of Programming/Getting Started/Grading System.java
@@ -0,0 +1,20 @@
+import java.util.*;
+  
+public class Main{
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int marks = scn.nextInt();
+    if(marks>90){
+        System.out.print("excellent");
+    }else if(marks>80){
+        System.out.print("good");
+    }else if(marks>70){
+        System.out.print("fair");
+    }else if(marks>60){
+        System.out.print("meets expectations");
+    }else{
+        System.out.print("below par");
+    }
+ }
+}
