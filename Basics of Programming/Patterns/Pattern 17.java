b6c334a2f7af48a6a5e4712cf95d98893421b2d6
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-02-26 23:51:13 +0530

    Create Pattern 17.java

---
+++ Basics of Programming/Patterns/Pattern 17.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int st=1,sp=n/2;
+        
+        for(int i=1;i<=n;i++){
+            if(i==n/2+1){
+                for(int j=1;j<=sp;j++){
+                    System.out.print("*\t");
+                }
+            }else{
+                for(int j=1;j<=sp;j++){
+                    System.out.print("\t");
+                }
+            }
+            
+            for(int j=1;j<=st;j++){
+                System.out.print("*\t");
+            }
+            if(i<=n/2){
+                st++;
+            }
+            else{
+                st--;
+            }
+            System.out.println();
+        }
+    }
+}
