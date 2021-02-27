c4ef9178ad5426eed0ebd774ce9fa51e77be5c22
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-02-27 23:43:34 +0530

    Create Find element in an Array.java

---
+++ Basics of Programming/Functions and Arrays/Find element in an Array.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=scn.nextInt();
+    }
+  
+    int d=scn.nextInt();
+    int found = -1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==d){
+            found=i;
+            break;
+        }
+    }
+    System.out.print(found);
+ }
+
+}
