90aded92e2a491834a07b74677018cf72e71afad
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-02-27 12:08:40 +0530

    Create Any base multiplication.java

---
+++ Basics of Programming/Functions and Arrays/Any base multiplication.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int b = scn.nextInt();
+    int n1 = scn.nextInt();
+    int n2 = scn.nextInt();
+
+    int d = getProduct(b, n1, n2);
+    System.out.println(d);
+ }
+
+ public static int getProduct(int b, int n1, int n2){
+     int num1 = getValueIndecimal(n1,b);
+     int num2 = getValueIndecimal(n2,b);
+     int ans = getValueInBase(num2*num1,b);
+     return ans;
+ }
+ 
+ public static int getValueIndecimal(int n, int b){
+      int sum=0,p=0;
+      while(n>0){
+          sum+=(n%10) * p;
+          p *= 10;
+          n /= 10;
+      }
+      return sum;
+   }
+   public static int getValueInBase(int n, int b){
+       int sum=0,p=1;
+       while(n>0){
+           sum+=(n%b)*p;
+           p *=10;
+           n/=b;
+       }
+       return sum;
+   }
+
+}
