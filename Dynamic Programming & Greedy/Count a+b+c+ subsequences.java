f6a55733269da1f1b40ce53d6f4917311487a199
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-03-06 00:26:31 +0530

    Create Count a+b+c+ subsequences.java

---
+++ Dynamic Programming & Greedy/Count a+b+c+ subsequences.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        
+        String s = scn.nextLine();
+        int l = s.length();
+        
+        int a = 0;
+        int ab = 0;
+        int abc = 0;
+        
+        for(int i = 0; i < l; i++)
+        {
+            char ch = s.charAt(i);
+            
+            if(ch == 'a')
+            {
+                a = 2 * a + 1;
+            }
+            else if(ch == 'b')
+            {
+                ab = 2 * ab + a;
+            }
+            else
+            {
+                abc = 2 * abc + ab;
+            }
+        }
+        
+        System.out.println(abc);
+    }
+}
