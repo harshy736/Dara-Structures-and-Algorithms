dea3ef44ffc4e637232fa7cd65cf23902d18ae7c
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-03-05 23:32:51 +0530

    Create Coin Change Permutation.java

---
+++ Dynamic Programming & Greedy/Coin Change Permutation.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int[] coins = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            coins[i] = scn.nextInt();
+        }
+        
+        int amt = scn.nextInt();
+        
+        int[] dp = new int[amt + 1];
+        dp[0] = 1;
+        
+        for(int j = 1; j < dp.length; j++)
+        {
+            for(int i = 0; i < coins.length; i++)
+            {
+                int c = coins[i];
+                if(c <= j)
+                {
+                    dp[j] += dp[j - c];
+                }
+            }
+        }
+        
+        
+        System.out.println(dp[amt]);
+    }
+}
