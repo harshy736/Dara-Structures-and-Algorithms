64e8caed9e93e9d6eee0fb66a09c0157dbc371d3
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-03-14 18:56:19 +0530

    Create Node With Maximum Subtree Sum.java

---
+++ Basic Data Structures/Generic Tree/Node With Maximum Subtree Sum.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+  
+  static int maxS;//max sum
+  static Node maxN;//node
+  public static int sumSubtree(Node node){
+      int sum = 0;
+      for(Node child : node.children){
+          int csum = sumSubtree(child);
+          sum += csum;
+      }
+      
+      sum += node.data;
+      if(maxS < sum){
+          maxS = sum;
+          maxN = node;
+      }
+      
+      return sum;
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    Node root = construct(arr);
+    maxS = 0;
+    maxN = null;
+    sumSubtree(root);
+    
+    System.out.println(maxN.data + "@" + maxS);
+    
+  }
+
+}
