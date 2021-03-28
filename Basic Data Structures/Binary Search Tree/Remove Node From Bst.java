b024ad53f77caf8ae0c4d650185e3984ec64d428
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-03-28 23:57:29 +0530

    Update Remove Node From Bst.java

--- Basic Data Structures/Binary Search Tree/Remove Node From Bst.java
+++ Basic Data Structures/Binary Search Tree/Remove Node From Bst.java
@@ -2,143 +2,135 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static class Node {
-        int data;
-        Node left;
-        Node right;
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
 
-        Node(int data, Node left, Node right) {
-            this.data = data;
-            this.left = left;
-            this.right = right;
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
         }
+
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
+        }
+
+        top.state++;
+      } else {
+        st.pop();
+      }
     }
 
-    public static class Pair {
-        Node node;
-        int state;
+    return root;
+  }
 
-        Pair(Node node, int state) {
-            this.node = node;
-            this.state = state;
-        }
+  public static void display(Node node) {
+    if (node == null) {
+      return;
     }
 
-    public static Node construct(Integer[] arr) {
-        Node root = new Node(arr[0], null, null);
-        Pair rtp = new Pair(root, 1);
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
 
-        Stack < Pair > st = new Stack < > ();
-        st.push(rtp);
+    display(node.left);
+    display(node.right);
+  }
+  
+  
+  public static Node max(Node node){
+      if(node.right == null){
+          return node;
+      }
+      
+      return max(node.right);
+  } 
 
-        int idx = 0;
-        while (st.size() > 0) {
-            Pair top = st.peek();
-            if (top.state == 1) {
-                idx++;
-                if (arr[idx] != null) {
-                    top.node.left = new Node(arr[idx], null, null);
-                    Pair lp = new Pair(top.node.left, 1);
-                    st.push(lp);
-                } else {
-                    top.node.left = null;
-                }
-
-                top.state++;
-            } else if (top.state == 2) {
-                idx++;
-                if (arr[idx] != null) {
-                    top.node.right = new Node(arr[idx], null, null);
-                    Pair rp = new Pair(top.node.right, 1);
-                    st.push(rp);
-                } else {
-                    top.node.right = null;
-                }
-
-                top.state++;
-            } else {
-                st.pop();
-            }
+  public static Node remove(Node node, int data) {
+    if(node.data > data){
+        node.left = remove(node.left, data);
+    }else if(node.data < data){
+        node.right = remove(node.right, data);
+    }else{//remove this node
+        if(node.left == null && node.right == null){// 0 child
+            return null;
+        }else if(node.left == null){
+            return node.right;
+        }else if(node.right == null){
+            return node.left;
+        }else{//2 child -> return left max and remove it from original position
+            Node lmax = max(node.left);
+            lmax.left = remove(node.left, lmax.data);
+            lmax.right = node.right;
+            
+            return lmax;
         }
-
-        return root;
-    }
-
-    public static void display(Node node) {
-        if (node == null) {
-            return;
-        }
-
-        String str = "";
-        str += node.left == null ? "." : node.left.data + "";
-        str += " <- " + node.data + " -> ";
-        str += node.right == null ? "." : node.right.data + "";
-        System.out.println(str);
-
-        display(node.left);
-        display(node.right);
-    }
-
-    public static int max(Node node) {
-        if (node == null) {
-            return Integer.MIN_VALUE;
-        }
-        int m = Math.max(max(node.right), node.data);
-        return m;
     }
     
-    public static Node remove(Node node, int data) {
-        if(node == null){
-            return node;
-        }
-        if(node.data==data){
-            if(node.left==null && node.right==null){
-                node = null;
-                return node;
-            } else if(node.left==null){
-                node = node.right;
-                return node;
-            } else if(node.right==null){
-                node = node.left;
-                return node;
-            } else{
-                int m = max(node.left);
-                node.data = m;
-                Node r = remove(node.left,m);
-                node.left = r;
-                return node;
-            }
-        } else if(node.data>data){
-            Node lr = remove(node.left,data);
-            node.left = lr;
-            return node;
-        } else if(node.data<data){
-            Node rr = remove(node.right,data);
-            node.right = rr;
-            return node;
-        }
-        return node;
+    return node;
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
     }
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Integer[] arr = new Integer[n];
-        String[] values = br.readLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            if (values[i].equals("n") == false) {
-                arr[i] = Integer.parseInt(values[i]);
-            } else {
-                arr[i] = null;
-            }
-        }
+    int data = Integer.parseInt(br.readLine());
 
-        int data = Integer.parseInt(br.readLine());
+    Node root = construct(arr);
+    root = remove(root, data);
 
-        Node root = construct(arr);
-        root = remove(root, data);
-
-        display(root);
-    }
+    display(root);
+  }
 
 }
